62ad1365295a911156ecbe4027ab72bba4f2f1d7
sai3010 <saiprasanth2007@gmail.com>
2016-09-15 19:51:10 +0530

    sidenav fixed

--- page.js
+++ page.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-     $('.button-collapse').sideNav();
-  });
 function newwindow(){
 	var show=document.getElementById("past");
 	var hid=document.getElementById("up");
 	show.style.position="absolute";
 	show.style.visibility="visible";
 	hid.style.visibility="hidden";
-	show.addClass("animated rollIn");
-}
+	$("#past").addClass("animated slideInleft");
+	$("#navpast").addClass("active");
+	$("#navup").removeClass("active");
+	}
+	
